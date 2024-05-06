2f686cc1bad02038a6c377d7010d797afee47cc7
anhtuan284 <peteralwaysloveu@gmail.com>
2024-05-07 00:36:55 +0700

    prescrition & view patient history

--- clinicapp/__init__.py
+++ clinicapp/__init__.py
@@ -11,7 +11,7 @@ import cloudinary
 
 app = Flask(__name__)
 app.secret_key = '^%^&%^(*^^&&*^$%((^^&$$&^'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/clinicdb?charset=utf8mb4" % quote('Admin@123')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/clinicdb?charset=utf8mb4" % quote('123456')
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 8
