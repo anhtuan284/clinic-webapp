bea4765df80751a29ee05f1ffede1e4efea529bb
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2024-05-07 02:55:03 +0700

    debug xong? list index out of range khi tao hoa don

--- clinicapp/__init__.py
+++ clinicapp/__init__.py
@@ -12,7 +12,7 @@ import cloudinary
 app = Flask(__name__)
 
 app.secret_key = '^%^&%^(*^^&&*^$%((^^&$$&^'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/clinicdb?charset=utf8mb4" % quote('Baopro123')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/clinicdb?charset=utf8mb4" % quote('Admin@123')
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 8
