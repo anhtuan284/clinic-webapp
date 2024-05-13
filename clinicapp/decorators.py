6c2eaeae87ccb38dc5c3e05e1e2969ff786eb22d
anhtuan284 <peteralwaysloveu@gmail.com>
2024-05-13 19:30:54 +0700

    resources owner deco + filter in patient's history

--- clinicapp/decorators.py
+++ clinicapp/decorators.py
@@ -49,3 +49,23 @@ def roles_required(roles):
         return decorated_function
 
     return decorator
+
+
+def resources_owner(resource_user_id_param='user_id', allowed_roles=None):
+    def decorator(func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            resource_user_id = kwargs.get(resource_user_id_param)
+
+            if allowed_roles and current_user.role not in allowed_roles:
+                abort(403)
+
+            if current_user.role == UserRole.PATIENT:
+                if current_user.id != resource_user_id:
+                    abort(403)
+
+            return func(*args, **kwargs)
+
+        return decorated
+
+    return decorator
